ae1f68a43c4699368a47c0416e76e138281a32d0
Félix Alberto Nieto García <71712844+Felix-07@users.noreply.github.com>
2024-02-10 18:06:36 -0600

    transform and load bit

--- Orchestrator/ros2_labremoto/src/py_pubsub/py_pubsub/SendTran_Service.py
+++ Orchestrator/ros2_labremoto/src/py_pubsub/py_pubsub/SendTran_Service.py
@@ -18,9 +18,12 @@ class SendTranDataBase(Node):
                     "id_user": request.id_user,
                     "log_process": request.log_process}
 
-        resp = requests.post('http://labrem.ddns.net/api/TranOrquestador/', data=InfoTran)
+        resp = requests.post('http://labrem.ddns.net/api/', data=InfoTran)
 
         return resp.status_code
+    
+    def get_info(self,tabla,request):
+        resp = requests.get(f'http://labrem.ddns.net:8000/api/{tabla}/1/',timeout=0.01)
 
 
 def main():
