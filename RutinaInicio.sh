2746dc2e02cebde8582f55ea9762f0b4ee80ad21
Félix Alberto Nieto García <71712844+Felix-07@users.noreply.github.com>
2024-03-04 08:09:24 -0600

    Update RutinaInicio.sh

--- RutinaInicio.sh
+++ RutinaInicio.sh
@@ -4,14 +4,11 @@ echo "Inicio del LABORATORIO REMOTO"
 source /opt/ros/humble/setup.bash
 echo "humble sourced ready"
 
-source /home/ffelix07/Documents/LabRemo/Orchestrator/ros2_labremoto/src/install/setup.sh
+source /home/trabajo/labremoto_ws/src/install/setup.sh
 echo "labremote sourced ready"
 
-
-conda activate humble
-
-ros2 launch /opt/ros/humble/share/rosbridge_server/launch/rosbridge_websocket_launch.xml
-echo "rosbridge ready"
+#ros2 launch /opt/ros/humble/share/rosbridge_server/launch/rosbridge_websocket_launch.xml
+#echo "rosbridge ready"
 
 ros2 launch py_pubsub partes.launch.py
 echo "partes de orquestador ready"
