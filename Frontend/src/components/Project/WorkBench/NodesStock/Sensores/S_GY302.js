609373d405bf7b923bd86d71e3764618a348f169
Amenao12 <americajimenezavila@gmail.com>
2025-06-27 14:32:47 -0600

    Agregaron pines de los sensores

--- Frontend/src/components/Project/WorkBench/NodesStock/Sensores/S_GY302.js
+++ Frontend/src/components/Project/WorkBench/NodesStock/Sensores/S_GY302.js
@@ -1,17 +1,48 @@
 import S_GY302 from "@/assets/media/Nodes/Sensores (L-O)/S_GY302.png";
 
+const defaultHandleSize = { width: 1, height: 1 };
+
 export default {
-  name: "GY302",
+  name: "s_GY302",
   url: S_GY302,
   type: "sensor",
   size: { x: "20%", y: "20%" },
   handles: [
     {
-      id: "OUT",
-      type: "source",
-      position: "left",
-      style: { left: "0em", top: "1.5em", background: "orange" },
+      id: "vcc",
+      type: "target",
+      position: "bottom",
+      style: { left: "20.5%", bottom: "0.7em", background: "orange" },
+      isConnectable: true,
+    },
+    {
+      id: "GND",
+      type: "target",
+      position: "bottom",
+      style: { left: "37%", bottom: "0.7em", background: "orange" },
+      isConnectable: true,
+    },
+    {
+      id: "SCL",
+      type: "target",
+      position: "bottom",
+      style: { left: "52.5%", bottom: "0.7em", background: "orange" },
+      isConnectable: true,
+    },
+    {
+      id: "SDA",
+      type: "target",
+      position: "bottom",
+      style: { left: "70.5%", bottom: "0.7em", background: "orange" },
+      isConnectable: true,
+    },
+    {
+      id: "ADDR",
+      type: "target",
+      position: "bottom",
+      style: { left: "87%", bottom: "0.7em", background: "orange" },
       isConnectable: true,
     },
   ],
 };
+
